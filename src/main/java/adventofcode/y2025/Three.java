7a84f27c1a795958a8dd01b867ce0989136cb39c
Sylvain Berthouzoz <sylvain.berthouzoz@soprasteria.com>
2025-12-03 15:38:17 +0100

    day 3: part2 work in progress

--- src/main/java/adventofcode/y2025/Three.java
+++ src/main/java/adventofcode/y2025/Three.java
@@ -18,12 +18,28 @@ public class Three {
             System.out.println("totalOutputJoltage = " + totalOutputJoltage);
         }
     }
+
     static int[] parseBank(String bank) {
         return bank.chars().map(c -> Character.digit(c, 10)).toArray();
     }
 
     static int maxOfBank(int[] bank, IndexOfMax indexes) {
         return bank[indexes.idx1()] * 10 + bank[indexes.idx2()];
+    }
+
+    static double maxOfBank(int[] bank, IndexOfMax12 indexes) {
+        return bank[indexes.idx1()] * 1e11
+                + bank[indexes.idx2()] * 1e10
+                + bank[indexes.idx3()] * 1e9
+                + bank[indexes.idx4()] * 1e8
+                + bank[indexes.idx5()] * 1e7
+                + bank[indexes.idx6()] * 1e6
+                + bank[indexes.idx7()] * 1e5
+                + bank[indexes.idx8()] * 1e4
+                + bank[indexes.idx9()] * 1e3
+                + bank[indexes.idx10()] * 1e2
+                + bank[indexes.idx11()] * 1e1
+                + bank[indexes.idx12()];
     }
 
     static IndexOfMax getIndexOfMax(int[] bank) {
@@ -43,6 +59,23 @@ public class Three {
         }
     }
 
+    /*static IndexOfMax12 getIndexOfMax12(int[] bank) {
+        var bankWithIdx = IntStream.range(0, bank.length)
+                .mapToObj(i -> new IndexOfMax(i, bank[i]))
+                .sorted(Comparator.comparingInt(IndexOfMax::idx2).reversed())
+                .toList();
+        if (bankWithIdx.getFirst().idx1() == bank.length - 1) { // the max number is at the end, can't be used as first
+            return new IndexOfMax12(bankWithIdx.get(1).idx1(), bankWithIdx.getFirst().idx1());
+        } else {
+            return new IndexOfMax(
+                    bankWithIdx.getFirst().idx1(),
+                    bankWithIdx.stream().filter(nextMax(bankWithIdx.getFirst()))
+                            .findFirst()
+                            .map(IndexOfMax::idx1)
+                            .orElseThrow(() -> new IllegalArgumentException("Nothing found after " + bankWithIdx.getFirst())));
+        }
+    }*/
+
     private static Predicate<IndexOfMax> nextMax(final IndexOfMax currentMax) {
         return iom -> iom.idx1() > currentMax.idx1();
     }
@@ -51,5 +84,13 @@ public class Three {
     }
 
     record BankAndIndexes(int[] bank, Three.IndexOfMax indexes) {
+    }
+
+    // part 2
+    record IndexOfMax12(int idx1, int idx2, int idx3, int idx4, int idx5, int idx6, int idx7, int idx8, int idx9,
+                        int idx10, int idx11, int idx12) {
+    }
+
+    record BankAndIndexes12(int[] bank, Three.IndexOfMax12 indexes) {
     }
 }
