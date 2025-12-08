6a3490939fce943e7109a037a5262d2752f991ae
Sylvain Berthouzoz <sylvain.berthouzoz@soprasteria.com>
2025-12-08 15:54:02 +0100

    day 6: part 1 passed

--- src/main/java/adventofcode/y2025/Six.java
+++ src/main/java/adventofcode/y2025/Six.java
@@ -2,16 +2,27 @@ package adventofcode.y2025;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.*;
 import java.util.function.Function;
 import java.util.function.LongBinaryOperator;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Six {
     private static final String OPERATOR_CHARS = "+*";
     private final List<Long> results;
     private final List<Operator> operators;
+
+    public static void main(String[] args) throws IOException {
+        var file = Path.of(args[0]);
+        var obj = Six.parse(file);
+        obj.partOne(file);
+        System.out.println("obj.summingResults() = " + obj.summingResults());
+    }
 
     public Six(List<Operator> operators, List<Long> longs) {
         this.operators = operators;
@@ -24,16 +35,35 @@ public class Six {
         return new Six(operators, new ArrayList<>());
     }
 
+    public static Six parse(Path input) throws IOException {
+        try (var lines = Files.lines(input)) {
+            var lineOper = lines.dropWhile(s -> !StringUtils.containsAny(s, OPERATOR_CHARS)).findFirst().orElseThrow();
+            var operators = Arrays.stream(StringUtils.split(lineOper)).map(Operator::valueOfChar).toList();
+            return new Six(operators, new ArrayList<>());
+        }
+    }
+
     public void partOne(String input) {
-        var firstline = input.lines().findFirst().orElseThrow();
-        Arrays.stream(StringUtils.split(firstline)).forEach(s -> results.add(Long.parseLong(s)));
-        input.lines().parallel()
-                .filter(s -> !firstline.equals(s))
+        var firstLine = input.lines().findFirst().orElseThrow();
+        partOne(firstLine, input.lines());
+    }
+
+    private void partOne(String firstLine, Stream<String> lines) {
+        Arrays.stream(StringUtils.split(firstLine)).forEach(s -> results.add(Long.parseLong(s)));
+        lines.parallel()
+                .filter(s -> !firstLine.equals(s))
                 .filter(s -> StringUtils.containsNone(s, OPERATOR_CHARS))
                 .forEach(line -> {
                     var splitted = StringUtils.split(line);
                     IntStream.range(0, splitted.length).parallel().forEach(idx -> results.set(idx, operators.get(idx).oper(results.get(idx), Long.parseLong(splitted[idx]))));
                 });
+    }
+
+    public void partOne(Path input) throws IOException {
+        try (var lines = Files.lines(input)) {
+            var firstLine = lines.findFirst().orElseThrow();
+            partOne(firstLine, Files.lines(input));
+        }
     }
 
     public long summingResults() {
