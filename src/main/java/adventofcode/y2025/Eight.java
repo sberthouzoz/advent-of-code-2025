30a67c342fe3c7f1b72b52c0e5175c73d394ee1f
Sylvain Berthouzoz <sylvain.berthouzoz@soprasteria.com>
2025-12-10 17:02:46 +0100

    day 8: part 1, f u

---
+++ src/main/java/adventofcode/y2025/Eight.java
@@ -0,0 +1,108 @@
+package adventofcode.y2025;
+
+import org.jspecify.annotations.NonNull;
+
+import java.util.*;
+import java.util.stream.Stream;
+
+public class Eight {
+    private final List<Point3D> input;
+    private final List<Point3DPairWithDistance> pairsWithDistance = new ArrayList<>();
+    private final CircuitSet circuits;
+
+    public Eight(Stream<String> input) {
+        this.input = input.parallel().map(line -> line.split(","))
+                .map(arr -> new Point3D(Integer.parseInt(arr[0]), Integer.parseInt(arr[1]), Integer.parseInt(arr[2])))
+                .toList();
+        circuits = new CircuitSet(this.input);
+        for (int i = 0; i < this.input.size(); i++) {
+            var point = this.input.get(i);
+
+            for (int j = i + 1; j < this.input.size(); j++) {
+                var point2 = this.input.get(j);
+                pairsWithDistance.add(new Point3DPairWithDistance(point, point2, point.distanceTo(point2)));
+            }
+        }
+        Collections.sort(pairsWithDistance);
+    }
+
+    public static long partOne(Stream<String> input, int iterations) {
+        var junctions = new Eight(input);
+
+        for (int i = 0; i < iterations; i++) {
+            var pair = junctions.pairsWithDistance.get(i);
+            junctions.circuits.add(pair);
+        }
+
+        var largest = junctions.circuits.getLargest(3);
+        return largest.stream().mapToInt(Set::size).reduce(1, (a, b) -> a * b);
+    }
+
+
+}
+
+record Point3D(int x, int y, int z) implements Comparable<Point3D> {
+    public static final Point3D ORIGIN = new Point3D(0, 0, 0);
+
+    @Override
+    public int compareTo(@NonNull Point3D o) {
+        return COMPARATOR.compare(this, o);
+    }    private static final Comparator<Point3D> COMPARATOR = Comparator.comparingDouble(p3d -> p3d.distanceTo(ORIGIN));
+
+    public double distanceTo(Point3D other) {
+        if (this.equals(other)) {
+            return 0;
+        }
+        var min = (other.equals(ORIGIN) || this.compareTo(other) <= 0) ? this : other;
+        var max = (this == min) ? other : this;
+        return Math.sqrt(Math.pow(max.x - min.x, 2) + Math.pow(max.y * min.y, 2) + Math.pow(max.z - min.z, 2));
+    }
+
+
+}
+
+record Point3DPairWithDistance(Point3D first, Point3D second,
+                               double distance) implements Comparable<Point3DPairWithDistance> {
+    @Override
+    public int compareTo(@NonNull Point3DPairWithDistance o) {
+        return Double.compare(this.distance, o.distance);
+    }
+}
+
+class CircuitSet {
+    private final List<Set<Point3D>> circuits = new ArrayList<>();
+
+    CircuitSet(List<Point3D> points) {
+        for (var point : points) {
+            var circuit = new HashSet<Point3D>();
+            circuit.add(point);
+            circuits.add(circuit);
+        }
+    }
+
+    void add(Point3DPairWithDistance pair) {
+        int firstIndex = -1;
+        int secondIndex = -1;
+
+        for (int i = 0; i < circuits.size(); i++) {
+            Set<Point3D> circuit = circuits.get(i);
+            if (circuit.contains(pair.first())) {
+                firstIndex = i;
+            }
+            if (circuit.contains(pair.second())) {
+                secondIndex = i;
+            }
+        }
+
+        // Connect circuits if both are in different ones
+        if (firstIndex != secondIndex) {
+            circuits.get(firstIndex).addAll(circuits.get(secondIndex));
+            circuits.remove(secondIndex);
+        }
+    }
+
+    public List<Set<Point3D>> getLargest(int limit) {
+        // TODO: reverse
+        return circuits.stream().sorted(Comparator.comparingInt(Set::size)).limit(3).toList();
+    }
+}
---
+++ src/test/java/adventofcode/y2025/EightTest.java
@@ -0,0 +1,36 @@
+package adventofcode.y2025;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class EightTest {
+    private String example = """
+            162,817,812
+            57,618,57
+            906,360,560
+            592,479,940
+            352,342,300
+            466,668,158
+            542,29,236
+            431,825,988
+            739,650,466
+            52,470,668
+            216,146,977
+            819,987,18
+            117,168,530
+            805,96,715
+            346,949,466
+            970,615,88
+            941,993,340
+            862,61,35
+            984,92,344
+            425,690,689""";
+
+    @Test
+    void partOne() {
+        var result = Eight.partOne(example.lines(), 10);
+
+        assertThat(result).isEqualTo(40);
+    }
+}
