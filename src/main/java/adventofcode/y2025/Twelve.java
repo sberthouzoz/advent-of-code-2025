c496b1f89403cd1483fd12cc66be9247ee18e061
Sylvain Berthouzoz <sylvain.berthouzoz@soprasteria.com>
2025-12-18 14:48:26 +0100

    day 12: part1 OK, example not solved and not required.

--- src/main/java/adventofcode/y2025/Twelve.java
+++ src/main/java/adventofcode/y2025/Twelve.java
@@ -1,18 +1,155 @@
 package adventofcode.y2025;
 
-import java.util.Arrays;
+import org.apache.commons.lang3.StringUtils;
+import org.jspecify.annotations.NonNull;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.IntStream;
 
 public class Twelve {
+    final List<Shape> shapes = new ArrayList<>();
+    final List<Region> regions = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        var path = Path.of(args[0]);
+        var input = Files.readString(path);
+
+        var obj = parse(input);
+        var start = Instant.now();
+        var part1 = obj.solvePart1();
+        var end = Instant.now();
+        System.out.println("Duration = " + Duration.between(start, end));
+        System.out.println("part1 = " + part1);
+    }
+
+    public static Twelve parse(String input) {
+        var parsed = new Twelve();
+        var lines = input.lines().filter(StringUtils::isNotBlank).toList();
+        var sb = new StringBuilder();
+        for (var line : lines) {
+            if (line.contains("x")) {
+                if (!sb.isEmpty()) {
+                    parsed.shapes.add(Shape.parse(sb.toString()));
+                    sb.setLength(0);
+                }
+                parsed.regions.add(Region.parse(line));
+                continue;
+            }
+            if (line.matches("^\\d+:")) {
+                if (!sb.isEmpty()) parsed.shapes.add(Shape.parse(sb.toString()));
+                sb.setLength(0);
+            } else {
+                sb.append("\n").append(line);
+            }
+        }
+        return parsed;
+    }
+
+    long solvePart1() {
+        return regions.stream()
+                .filter(hasEnoughSpace())
+                .filter(fitsEnoughPresentRegardlessOfShape())
+                .count();
+    }
+
+    private Predicate<? super Region> fitsEnoughPresentRegardlessOfShape() {
+        return r -> {
+            var numberOfPresentsThatWouldFitRegardlessOfTheirShape = (r.width / 3) * (r.height / 3);
+            var nbPresentsToFit = Arrays.stream(r.requiredShapes).sum();
+            return nbPresentsToFit <= numberOfPresentsThatWouldFitRegardlessOfTheirShape;
+        };
+    }
+
+    private Predicate<? super Region> hasEnoughSpace() {
+        return r -> {
+            var size = r.height * r.width;
+            var totalPixel = IntStream.range(0, r.requiredShapes.length).map(i -> shapes.get(i).getSizeInPx() * r.requiredShapes[i]).sum();
+            return size >= totalPixel;
+        };
+    }
 
     record Shape(boolean[][] shape) {
+        private static final char FILLED = '#';
+
+        public static Shape parse(String input) {
+            var lines = input.lines().toList();
+            var arr = new boolean[lines.getFirst().length()][lines.size()];
+            for (int i = 0; i < arr.length; i++) {
+                var line = lines.get(i);
+                for (int j = 0; j < line.length(); j++) {
+                    arr[i][j] = line.charAt(j) == FILLED;
+                }
+            }
+            return new Shape(arr);
+        }
+
+        public int getSizeInPx() {
+            return (int) Arrays.stream(shape).flatMap(arr -> IntStream.range(0, arr.length).mapToObj(i -> arr[i]))
+                    .filter(Boolean.TRUE::equals).count();
+        }
         Shape rotateLeft() {
-            var clone = new Shape(shape.clone());
-            return clone;
+            return rotateRight().rotateRight().rotateRight();
         }
 
         Shape rotateRight() {
-            var clone = new Shape(shape.clone());
+            var clone = new Shape(deepCopy(shape));
+            int dim = clone.shape.length;
+
+            for (int i = 0; i <= (dim - 1) / 2; i++) {
+                for (int j = i; j < dim - i - 1; j++) {
+                    var p1 = clone.shape[i][j];
+                    var p2 = clone.shape[j][dim - i - 1];
+                    var p3 = clone.shape[dim - i - 1][dim - j - 1];
+                    var p4 = clone.shape[dim - j - 1][i];
+
+                    clone.shape[j][dim - i - 1] = p1;
+                    clone.shape[dim - i - 1][dim - j - 1] = p2;
+                    clone.shape[dim - j - 1][i] = p3;
+                    clone.shape[i][j] = p4;
+                }
+            }
             return clone;
+        }
+
+        private boolean[][] deepCopy(boolean[][] arr) {
+            boolean[][] copy = new boolean[arr.length][];
+            for (int i = 0; i < arr.length; i++) {
+                copy[i] = arr[i].clone();
+            }
+            return copy;
+        }
+
+        @Override
+        public @NonNull String toString() {
+            var sb = new StringBuilder(shape.length * shape.length + shape.length);
+            for (boolean[] bools : shape) {
+                for (int j = 0; j < shape.length; j++) {
+                    sb.append(bools[j] ? FILLED : '.');
+                }
+                sb.append("\n");
+            }
+            sb.deleteCharAt(sb.length() - 1);
+            return new StringJoiner(", ", Shape.class.getSimpleName() + "[", "]")
+                    .add("shape=" + Arrays.deepToString(shape))
+                    .add("displayString=" + sb)
+                    .toString();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (!(o instanceof Shape(boolean[][] shape2))) return false;
+            return Objects.deepEquals(shape, shape2);
+        }
+
+        @Override
+        public int hashCode() {
+            return Arrays.deepHashCode(shape);
         }
     }
 
@@ -29,6 +166,17 @@ public class Twelve {
             var height = Integer.parseInt(input, xIndex + 1, colonIndex, radix_ten);
             var requiredShapes = Arrays.stream(input.substring(colonIndex + 2).split(" ")).mapToInt(Integer::parseInt).toArray();
             return new Region(width, height, requiredShapes);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (!(o instanceof Region region)) return false;
+            return width == region.width && height == region.height && Objects.deepEquals(requiredShapes, region.requiredShapes);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(width, height, Arrays.hashCode(requiredShapes));
         }
     }
 }
--- src/test/java/adventofcode/y2025/TwelveTest.java
+++ src/test/java/adventofcode/y2025/TwelveTest.java
@@ -1,11 +1,93 @@
 package adventofcode.y2025;
 
 import adventofcode.y2025.Twelve.Region;
+import adventofcode.y2025.Twelve.Shape;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class TwelveTest {
+    static final String EXAMPLE = """
+            0:
+            ###
+            ##.
+            ##.
+            
+            1:
+            ###
+            ##.
+            .##
+            
+            2:
+            .##
+            ###
+            ##.
+            
+            3:
+            ##.
+            ###
+            ##.
+            
+            4:
+            ###
+            #..
+            ###
+            
+            5:
+            ###
+            .#.
+            ###
+            
+            4x4: 0 0 0 0 2 0
+            12x5: 1 0 1 0 2 2
+            12x5: 1 0 1 0 3 2""";
+
+    @Test
+    void parse() {
+        var res = Twelve.parse(EXAMPLE);
+
+        assertThat(res).isNotNull();
+        assertThat(res.shapes).hasSize(6);
+        assertThat(res.regions).hasSize(3);
+    }
+
+    @Test
+    void part1() {
+        var res = Twelve.parse(EXAMPLE);
+
+        var part1 = res.solvePart1();
+        assertThat(part1).isEqualTo(0);
+    }
+
+    @Test
+    void parseShape() {
+        var input = """
+                ###
+                #..
+                ###""";
+
+        var parsed = Shape.parse(input);
+
+        assertThat(parsed).isNotNull();
+        assertThat(parsed.shape()).hasDimensions(3, 3);
+        assertThat(parsed.shape()[0]).containsExactly(true, true, true);
+        assertThat(parsed.shape()[1]).containsExactly(true, false, false);
+        assertThat(parsed.shape()[2]).containsExactly(true, true, true);
+
+        assertThat(parsed.getSizeInPx()).isEqualTo(input.chars().filter(c -> c == '#').count());
+        assertThat(parsed.toString()).contains(input);
+
+        var rotated = parsed.rotateRight();
+        assertThat(rotated).isNotNull();
+        assertThat(rotated).isNotEqualTo(parsed);
+        assertThat(rotated.getSizeInPx()).isEqualTo(parsed.getSizeInPx());
+        assertThat(rotated.shape()[0]).containsExactly(true, true, true);
+        assertThat(rotated.shape()[1]).containsExactly(true, false, true);
+        assertThat(rotated.shape()[2]).containsExactly(true, false, true);
+
+        assertThat(parsed.rotateLeft().rotateRight()).isEqualTo(parsed);
+    }
+
     @Test
     void parseRegion() {
         var input = "12x5: 1 0 1 0 2 2";
